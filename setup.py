d409189c8b6484e81a90fc2f513e7fe6a4a71a40
Minh-Tri <pmtri80@gmail.com>
2019-04-03 10:23:31 +0100

    kivy setup

--- setup.py
+++ setup.py
@@ -2,25 +2,19 @@
 
 from setuptools import setup, find_packages
 
-setup(name='muv1',
-      version='0.4.1',
-      description="Winnow Vision dataset loader and viewer",
-      author=["Phong Dinh Vo", "Minh-Tri Pham"],
-      scripts=['scripts/visionml_viewer.py'],
+setup(name='kivy_mt',
+      version='0.0.1',
+      description="Extra Kivy modules written by Minh-Tri Pham",
+      author=["Minh-Tri Pham"],
+      #scripts=['scripts/visionml_viewer.py'],
       packages=find_packages(),
-      package_data={
-        'muv1.viewer': ['data/*'],
-      },
-      include_package_data=True,
+      #package_data={
+      #  'muv1.viewer': ['data/*'],
+      #},
+      #include_package_data=True,
       zip_safe=False,
       install_requires=[
-        'mlcore>=0.4.6',
-        'numpy',
-        'pandas',
-        'Cython', # for taxtree
-        #'sshtunnel', # for accessing Winnow home
-        #'pygame', # for kivy
-        #'kivy', # should still work without kivy
-        #'tornado', # for rapidly serving event images
+        'kivy',
+        'kivy-garden',
       ]
     )
