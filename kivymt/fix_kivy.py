57c3e2e67e8f79e262913bfd4fc699372a5361a7
Minh-Tri Pham <minhtri.pham@winnowsolutions.com>
2019-05-23 23:25:41 +0700

    fix kivy on windows

--- kivymt/fix_kivy.py
+++ kivymt/fix_kivy.py
@@ -5,37 +5,40 @@
 
 
 import kivy.lang.builder
+import os
 
 
-def BuilderBase_load_file(self, filename, file_encoding='utf-8', **kwargs):
-    '''Insert a file into the language builder and return the root widget
-    (if defined) of the kv file.
+if os.name == 'nt': # Windows
 
-    :parameters:
-        `rulesonly`: bool, defaults to False
-            If True, the Builder will raise an exception if you have a root
-            widget inside the definition.
-        `file_encoding`: str, defaults to 'utf-8'
-            the encoding of the file
-    '''
-    filename = kivy.lang.builder.resource_find(filename) or filename
-    if __debug__:
-        kivy.lang.builder.trace('Lang: load file %s' % filename)
-    with open(filename, 'r', encoding=file_encoding) as fd:
-        kwargs['filename'] = filename
-        data = fd.read()
+    def BuilderBase_load_file(self, filename, file_encoding='utf-8', **kwargs):
+        '''Insert a file into the language builder and return the root widget
+        (if defined) of the kv file.
 
-        # remove bom ?
-        if kivy.lang.builder.PY2:
-            if data.startswith((kivy.lang.builder.codecs.BOM_UTF16_LE, kivy.lang.builder.codecs.BOM_UTF16_BE)):
-                raise ValueError('Unsupported UTF16 for kv files.')
-            if data.startswith((kivy.lang.builder.codecs.BOM_UTF32_LE, kivy.lang.builder.codecs.BOM_UTF32_BE)):
-                raise ValueError('Unsupported UTF32 for kv files.')
-            if data.startswith(kivy.lang.builder.codecs.BOM_UTF8):
-                data = data[len(kivy.lang.builder.codecs.BOM_UTF8):]
+        :parameters:
+            `rulesonly`: bool, defaults to False
+                If True, the Builder will raise an exception if you have a root
+                widget inside the definition.
+            `file_encoding`: str, defaults to 'utf-8'
+                the encoding of the file
+        '''
+        filename = kivy.lang.builder.resource_find(filename) or filename
+        if __debug__:
+            kivy.lang.builder.trace('Lang: load file %s' % filename)
+        with open(filename, 'r', encoding=file_encoding) as fd:
+            kwargs['filename'] = filename
+            data = fd.read()
 
-        return self.load_string(data, **kwargs)
+            # remove bom ?
+            if kivy.lang.builder.PY2:
+                if data.startswith((kivy.lang.builder.codecs.BOM_UTF16_LE, kivy.lang.builder.codecs.BOM_UTF16_BE)):
+                    raise ValueError('Unsupported UTF16 for kv files.')
+                if data.startswith((kivy.lang.builder.codecs.BOM_UTF32_LE, kivy.lang.builder.codecs.BOM_UTF32_BE)):
+                    raise ValueError('Unsupported UTF32 for kv files.')
+                if data.startswith(kivy.lang.builder.codecs.BOM_UTF8):
+                    data = data[len(kivy.lang.builder.codecs.BOM_UTF8):]
+
+            return self.load_string(data, **kwargs)
 
 
-kivy.lang.builder.BuilderBase._load_file = kivy.lang.builder.BuilderBase.load_file
-kivy.lang.builder.BuilderBase.load_file = BuilderBase_load_file
+    kivy.lang.builder.BuilderBase._load_file = kivy.lang.builder.BuilderBase.load_file
+    kivy.lang.builder.BuilderBase.load_file = BuilderBase_load_file
