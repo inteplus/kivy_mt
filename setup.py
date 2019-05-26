61e7736d0af5cae1e969df42f3d495fc01f7d8f6
Minh-Tri Pham <minhtri.pham@winnowsolutions.com>
2019-05-26 08:49:32 +0700

    update setup

--- setup.py
+++ setup.py
@@ -14,24 +14,25 @@ if os.name == 'nt': # windows
 else:
     preinstall_requires = []
 
-setup(name='kivymt',
-      version='0.0.1',
-      description="Extra Kivy modules written by Minh-Tri Pham",
-      author=["Minh-Tri Pham"],
-      #scripts=['scripts/visionml_viewer.py'],
-      packages=find_packages(),
-      package_data={
+setup(
+    name='kivymt',
+    version='0.0.1',
+    description="Extra Kivy modules written by Minh-Tri Pham",
+    author=["Minh-Tri Pham"],
+    #scripts=['scripts/visionml_viewer.py'],
+    packages=find_packages(),
+    package_data={
         'kivymt': ['data/*'],
-      },
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=preinstall_requires+[
+    },
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=preinstall_requires+[
         'basemt', # for logging and multi-threading purposes
         'kivy',
         'kivy-garden',
         'pygame', # for kivy
-      ],
-      dependency_links=[
-        'git+https://github.com/inteplus/basemt#egg=basemt-0.0.1',
-      ]
-    )
+    ],
+    dependency_links=[
+        'https://github.com/inteplus/basemt/tarball/master#egg=basemt-0.0.2',
+    ]
+)
